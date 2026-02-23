b001c3efb17cb9a560a326c5d46cd9652a896e0a
NekoMonci12 <monci@yuemi.org>
2026-02-23 11:14:46 +0000

    Remvoe Unused Routes

--- routes.php
+++ routes.php
@@ -5,7 +5,3 @@ use Paymenter\Extensions\Servers\DownloadableProducts\DownloadableProducts;
 
 Route::get('/downloadable-products/download/{service}', [DownloadableProducts::class, 'download'])
     ->name('service.download');
-
-Route::delete('/admin/download-logs/clear', [App\Http\Controllers\DownloadLogController::class, 'clear'])
-    ->name('download-logs.clear')
-    ->middleware('admin'); // Add appropriate middleware
